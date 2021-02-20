44416a313b59947fb506d4cb95017efccc4d44a4
Camilla Staunstrup <camilla.valerius@gmail.com>
2021-02-20 17:20:12 +0100

    quickUnion following book, with notes

--- Assignment2/src/com/company/QuickUnion.java
+++ Assignment2/src/com/company/QuickUnion.java
@@ -1,28 +1,46 @@
 package com.company;
 
 public class QuickUnion implements UnionFind {
-    private int[] id;
-    private int count;
+    private int[] id;               // access to component id (site indexed)
+    private int count;              // number of components
 
     public QuickUnion(int size) {
-        this.id = new int[size];
-        count = size;
-        for (int i = 0; i < size; i++) id[i] = i;
+        this.id = new int[size];    // Initialize component id array
+        count = size;               // Initialize count to size
+        for (int i = 0; i < size; i++) {
+            id[i] = i;
+        }
     }
 
     @Override
+    /**
+     * follow links to find the roots associated with p and q,
+     * then rename one of the components by linking one of these
+     * roots to the other
+     */
     public void union(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+        if (pRoot == qRoot) return; //if(connected(p, q)) return;
+        id[pRoot] = qRoot;
+        count--;
+    }
 
+    @Override
+    /**
+     * start at p, follow its link to another site and so
+     * forth until reaching a site that has a link to itself (root)
+     */
+    public int find(int p) {
+        while (p != id[p]) {
+            p = id[p];
+        }
+        return p;
     }
 
     @Override
     public int count() {
         return count;
-    }
-
-    @Override
-    public int find(int p) {
-        return 0;
     }
 
     @Override
--- Assignment2/src/com/company/UnionFind.java
+++ Assignment2/src/com/company/UnionFind.java
@@ -1,8 +1,8 @@
 package com.company;
 
 public interface UnionFind {
-    void union(int p, int q);                   // unite points p and q
-    int count();                                // How many sets are there
-    int find(int p);                            // given point p, return set of p
-    boolean connected(int p, int q);   // are the points p and q in the same set
+    void union(int p, int q);                   // add connection between p and q
+    int count();                                // number of components/sets
+    int find(int p);                            // component identifier for p ( given point p, return set of p)
+    boolean connected(int p, int q);            // return true if p and q are in the same component/set
 }
