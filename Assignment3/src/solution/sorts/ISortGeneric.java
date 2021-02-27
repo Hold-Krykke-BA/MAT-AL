b1e783e0f910cf4ecbe0c7c3608dcfab89e0f32c
Runi <Runiniclasen@hotmail.com>
2021-02-27 19:26:43 +0100

    add print methods

--- Assignment3/src/solution/sorts/ISortGeneric.java
+++ Assignment3/src/solution/sorts/ISortGeneric.java
@@ -3,6 +3,46 @@ package solution.sorts;
 import java.util.Comparator;
 
 public interface ISortGeneric {
-    <T> void sort(Comparator<T> comp, T[]array);            // Sort, different for each algorithm
-    <T> boolean isSorted(Comparator<T> comp, T[]array);     // Test if array is sorted
+    /**
+     * Sort method of choice
+     *
+     * @param comp  comparator of choice
+     * @param array array of type of choice
+     */
+    <T> void sort(Comparator<T> comp, T[] array);
+
+    /**
+     * Check if array is sorted using comparator of choice
+     *
+     * @param comp  comparator of choice
+     * @param array array of type of choice
+     * @return
+     */
+    <T> boolean isSorted(Comparator<T> comp, T[] array);
+
+    /**
+     * Prints an arrays contents
+     *
+     * @param array The array to be printed
+     */
+    default <T> void print(T[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    /**
+     * Prints an arrays contents with an upper limit
+     *
+     * @param array The array to be printed
+     * @param max   The maximum amount of values to be printed
+     */
+    default <T> void print(T[] array, int max) {
+        int cutoff = max < array.length ? max : array.length;
+        for (int i = 0; i < cutoff; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
 }
