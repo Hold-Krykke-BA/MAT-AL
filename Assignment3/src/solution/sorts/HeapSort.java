d50cf0f32afbb994c88450d15c3d60cd0f1fd122
Camilla Staunstrup <camilla.valerius@gmail.com>
2021-02-28 12:45:46 +0100

    heapsort beginning

--- Assignment3/src/solution/sorts/HeapSort.java
+++ Assignment3/src/solution/sorts/HeapSort.java
@@ -8,39 +8,54 @@ public class HeapSort implements ISortGeneric{
     private int left;
     private int right;
 
-    // unsorted
-    // {8, 2, 6, 4, 0, 3}
-    // index is in parenthesis
-    //        8(0)
-    //       /    \
-    //      /      \
-    //     2(1)     6(2)
-    //    /  \      /
-    //   4(3) 0(4) 3(5)
-    //
-    // sorted
-    // {0, 2, 3, 4, 6, 8}
-    // index is in parenthesis
-    //        0(0)
-    //       /    \
-    //      /      \
-    //     2(1)     3(2)
-    //    /  \      /
-    //   4(3) 6(4) 8(5)
-
+    /**
+     * Binary Heap is a complete tree (All levels are completely filled except possibly the last level and the last level has all keys as left as possible).
+     *
+     * Links for readme:
+     *
+     * build heap explanation
+     * https://stackoverflow.com/a/52613602
+     *
+     * leaf structure
+     * https://www.geeksforgeeks.org/leaf-starting-point-binary-heap-data-structure/
+     *
+     *  unsorted
+     *  {8, 2, 6, 4, 0, 3}
+     *  index is in parenthesis
+     *         8(0)
+     *        /    \
+     *       /      \
+     *      2(1)     6(2)
+     *     /  \      /
+     *    4(3) 0(4) 3(5)
+     *
+     *  after heapify (recursively)
+     *  {0, 2, 3, 4, 6, 8}
+     *  index is in parenthesis
+     *         0(0)
+     *        /    \
+     *       /      \
+     *      2(1)     3(2)
+     *     /  \      /
+     *    4(3) 6(4) 8(5)
+     */
+    
     @Override
     public <T> void sort(Comparator<T> comp, T[] array) {
-
+        heapSize = array.length;
+        buildHeap(array, comp);
     }
 
-    private void buildHeap(int[] array, Comparator comp) {
-        heapSize = array.length;
-        for (int i = heapSize / 2 - 1; i >= 0; i--) {
+    private <T> void buildHeap(T[] array, Comparator comp) {
+        // Greatest node position. Further positions are leaves (no children).
+        int startIdx = (heapSize / 2) - 1;
+
+        for (int i = startIdx; i >= 0; i--) {
             heapify(array, i, comp);
         }
     }
 
-    private void heapify(int[] array, int i, Comparator comp) {
+    private <T> void heapify(T[] array, int i, Comparator comp) {
     }
 
     private  int getLeft(int i) {
