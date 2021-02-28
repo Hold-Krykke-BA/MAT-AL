d65f81bec46146feb1560a24e7c3d81484b79008
Camilla Staunstrup <camilla.valerius@gmail.com>
2021-02-28 17:50:14 +0100

    heapsort with generics ed1

--- Assignment3/src/solution/Main.java
+++ Assignment3/src/solution/Main.java
@@ -1,7 +1,39 @@
 package solution;
 
+import solution.sorts.HeapSort;
+
+import java.util.Comparator;
+
 public class Main {
     public static void main(String[] args) {
-        
+        HeapSort sorter = new HeapSort();
+
+        Integer[] arrayI = {7, 4, 98, 3, 45, 3, 1, 45, 2, 7, 65, 12, 8, 77, 6};
+        Double[] arrayD = {7.5, 4.4, 98.0, 3.2, 45.9, 3.4, 1.7, 45.1, 2.6, 7.4, 65.8, 12.1, 8.2, 77.3, 6.4};
+        String[] arrayS = {"elephant", "paper", "drink", "glass", "lamp", "moon", "saturn", "venus", "sorter", "magic", "drill", "pace", "elastic", "globe", "gloss" };
+
+        print(arrayI);
+        System.out.println("Sorting Integer Array");
+        sorter.sort(Comparator.naturalOrder(), arrayI);
+        print(arrayI);
+
+        print(arrayD);
+        System.out.println("Sorting Double Array");
+        sorter.sort(Comparator.naturalOrder(), arrayD);
+        print(arrayD);
+
+        print(arrayS);
+        System.out.println("Sorting String Array");
+        sorter.sort(Comparator.naturalOrder(), arrayS);
+        print(arrayS);
+        System.out.println(sorter.isSorted(Comparator.naturalOrder(), arrayS));
+
+    }
+    private static <T> void print(T[] array){
+        System.out.println("***********************************");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(i + ": " + array[i]);
+        }
+        System.out.println("***********************************");
     }
 }
--- Assignment3/src/solution/sorts/HeapSort.java
+++ Assignment3/src/solution/sorts/HeapSort.java
@@ -4,14 +4,15 @@ import java.util.Comparator;
 
 public class HeapSort implements ISortGeneric{
     private int heapSize;
-    private int node;
-    private int left;
-    private int right;
 
     /**
      * Binary Heap is a complete tree (All levels are completely filled except possibly the last level and the last level has all keys as left as possible).
      *
      * Links for readme:
+     *
+     * heap sort:
+     * https://www.baeldung.com/java-heap-sort
+     * https://www.geeksforgeeks.org/heap-sort/
      *
      * build heap explanation
      * https://stackoverflow.com/a/52613602
@@ -32,22 +33,34 @@ public class HeapSort implements ISortGeneric{
      *  after heapify (recursively)
      *  {0, 2, 3, 4, 6, 8}
      *  index is in parenthesis
-     *         0(0)
+     *         08(0)
      *        /    \
      *       /      \
-     *      2(1)     3(2)
+     *      6(1)     3(2)
      *     /  \      /
-     *    4(3) 6(4) 8(5)
+     *    4(3) 2(4) 0(5)
      */
-    
+
     @Override
     public <T> void sort(Comparator<T> comp, T[] array) {
         heapSize = array.length;
         buildHeap(array, comp);
+
+        T temp;
+
+        // "removal" (*) of the root node until the heap is empty
+        // * putting last in the array
+        for (int i = array.length - 1; i >= 0; i--) {
+            temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+            heapSize--;
+            heapify(array, 0, comp);
+        }
     }
 
     private <T> void buildHeap(T[] array, Comparator comp) {
-        // Greatest node position. Further positions are leaves (no children).
+        // Greatest possible node position. Further positions are leaves (no children).
         int startIdx = (heapSize / 2) - 1;
 
         for (int i = startIdx; i >= 0; i--) {
@@ -56,22 +69,71 @@ public class HeapSort implements ISortGeneric{
     }
 
     private <T> void heapify(T[] array, int i, Comparator comp) {
+        // assume i is largest
+        int node = i;
+
+        // if left child is largest, swap node to left child
+        if ((leftChild(i) <= heapSize - 1) && (comp.compare(array[leftChild(i)], array[node]) > 0)) {
+            node = leftChild(i);
+        }
+
+        // if right child is largest, swap node to left child
+        if ((rightChild(i) <= heapSize - 1) && (comp.compare(array[rightChild(i)], array[node]) > 0)) {
+            node = rightChild(i);
+        }
+
+        // if node is not largest, recursively heapify subtree
+        // if node is largest, retrun to next loop in sort()
+        if (node != i) {
+            T temp = array[i];
+            array[i] = array[node];
+            array[node] = temp;
+            heapify(array, node, comp);
+        }
     }
 
-    private  int getLeft(int i) {
-        return 0;
+    /**
+     * children position in relation to parent
+     *           parent(i)
+     *         /        \
+     *        /          \
+     *    left(2*i+1)  right(2*i+2)
+     *
+     * Example:
+     *           parent(3)
+     *          /       \
+     *         /         \
+     *    left(7)    right(8)
+     *    (2*3+1)    (2*3+2)
+    * */
+    private  int leftChild(int i) {
+        return (2 * i) + 1;
     }
 
-    private  int getRight(int i) {
-        return 0;
+    private  int rightChild(int i) {
+        return (2 * i) + 2;
     }
 
-    private  int getParent(int i) {
-        return 0;
+    /**
+     * parents position in relation to child is: (i-1)/2
+     *
+     * Example:
+     *           parent(3)
+     *          /       \
+     *         /         \
+     *    left(7)    right(8)
+     *    (7-1)/2    (7-1)/2  (integer division is rounded down)
+     * */
+
+    private  int parent(int i) {
+        return (i-1)/2;
     }
 
     @Override
     public <T> boolean isSorted(Comparator<T> comp, T[] array) {
-        return false;
+        for (int i = 0; i < array.length-1; i++) {
+            if (comp.compare(array[i], array[i + 1]) > 0) return false;
+        }
+        return true;
     }
 }
