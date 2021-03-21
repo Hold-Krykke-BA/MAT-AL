0729f0a20dbafaafc6d2f3ad366f5249cbc5c2e4
Runi <Runiniclasen@hotmail.com>
2021-03-21 21:09:09 +0100

    cleanup

--- Assignment5/src/Main.java
+++ Assignment5/src/Main.java
@@ -3,21 +3,22 @@ import solution.Automaton;
 import solution.StateABCDEFG;
 import utils.Reader;
 
-import java.io.*;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
 
 public class Main {
+    private static final String pathAsger = "C:\\Users\\regsa\\OneDrive\\Desktop\\Softwareudvikling\\assignments\\MAL\\MAT-AL\\Assignment5\\src\\data\\logfile.txt";
+    private static final String pathRuni = "src/data/logfile.txt";
+    private static final String pathCamilla = "";
 
     public static void Controller() {
         Automaton automaton = new Automaton();
         Reader reader = new Reader();
-        HashMap<String, ArrayList<String>> logEntries = reader.ScanFile("C:\\Users\\regsa\\OneDrive\\Desktop\\Softwareudvikling\\assignments\\MAL\\MAT-AL\\Assignment5\\src\\data\\logfile.txt");
+        HashMap<String, ArrayList<String>> logEntries = reader.ScanFile(pathRuni);
 
+        System.out.println("------");
         //Print grouped log entries
+        System.out.println("Values read from logfile");
         for (HashMap.Entry<String, ArrayList<String>> entry : logEntries.entrySet()) {
             String key = entry.getKey();
             ArrayList<String> value = entry.getValue();
@@ -25,14 +26,14 @@ public class Main {
         }
 
         System.out.println("------");
-
         //Check if actions is legal on instance ID
+        System.out.println("Are the performed actions allowed?");
         for (HashMap.Entry<String, ArrayList<String>> entry : logEntries.entrySet()) {
             String key = entry.getKey();
             ArrayList<String> value = entry.getValue();
             IState state = new StateABCDEFG(0, false);
             System.out.println("key: " + key);
-            for(int i = 0; i < value.size(); i++) {
+            for (int i = 0; i < value.size(); i++) {
                 char action = value.get(i).toCharArray()[0];
                 System.out.println("state " + state.getIndex() + ", action " + action + ": " + automaton.Ok(state, action));
                 state = automaton.nextState(state, action);
@@ -40,7 +41,6 @@ public class Main {
         }
 
         System.out.println("------");
-
         //List of instances running and "stuck" in non final state
         ArrayList<String> instanceID = new ArrayList<>();
         for (HashMap.Entry<String, ArrayList<String>> entry : logEntries.entrySet()) {
@@ -59,9 +59,10 @@ public class Main {
 
     public static void main(String[] args) {
         Automaton automaton = new Automaton();
+        System.out.println("Printing Automaton");
         for (int i = 0; i < automaton.getTable().length; i++) {
-            for(int j = 0; j < automaton.getTable()[i].length; j++) {
-                if(automaton.getTable()[i][j] == null) {
+            for (int j = 0; j < automaton.getTable()[i].length; j++) {
+                if (automaton.getTable()[i][j] == null) {
                     System.out.print(0 + " ");
                 } else {
                     System.out.print(automaton.getTable()[i][j].getIndex() + " ");
@@ -69,15 +70,10 @@ public class Main {
             }
             System.out.println();
         }
-
-
         Controller();
-
-
 //        System.out.println(automaton.Ok(new StateABCDEFG(0, false), 'A'));
 //        System.out.println(automaton.nextState(new StateABCDEFG(1, false), 'A'));
 //        System.out.println(automaton.nextState(new StateABCDEFG(1, false), 'B').getIndex());
 //
-
     }
 }
--- Assignment5/src/solution/Automaton.java
+++ Assignment5/src/solution/Automaton.java
@@ -7,27 +7,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-
 public class Automaton implements IAutomaton {
     // Accept decoded alphabet (rules)?
     // Accept grouped logfiles
-    // determine
-        // is the action legal according to the present state
-        // is any instance stuck at a non-final (non-accepting) state
-        // give a list of the running instances
+    // determine:
+    // is the action legal according to the present state
+    // is any instance stuck at a non-final (non-accepting) state
+    // give a list of the running instances
 
     private List<IState> states = new ArrayList<>();
     private final IState[][] table;
     private IState initialState;
 
     //AB(C(D|E))|F*G
-    public Automaton(){
+    public Automaton() {
         states.add(initialState = new StateABCDEFG(0, false));
         for (int index = 1; index < 6; index++) {
             if (index == 4) states.add(new StateABCDEFG(index, true));
             else states.add(new StateABCDEFG(index, false));
         }
-        table = new IState[states.size()-1][7];
+        table = new IState[states.size() - 1][7];
         table[0][0] = states.get(1);
         table[1][1] = states.get(2);
         table[1][5] = states.get(1);
@@ -49,12 +48,12 @@ public class Automaton implements IAutomaton {
 
     @Override
     public List<IState> getStates() {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public IState getInitialState() {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
@@ -63,7 +62,8 @@ public class Automaton implements IAutomaton {
     }
 
     private int indexOfSymbol(char symbol) {
-        if (Character.toLowerCase(symbol) < 'a' || 'g' < Character.toLowerCase(symbol)) throw new IllegalArgumentException();
-        return Character.toLowerCase(symbol) - 'a';
+        symbol = Character.toLowerCase(symbol);
+        if (symbol < 'a' || 'g' < symbol) throw new IllegalArgumentException();
+        return symbol - 'a';
     }
 }
