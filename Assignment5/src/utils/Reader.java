753db1ca2172b0d43489f6075320232410e5bb86
Runi <Runiniclasen@hotmail.com>
2021-03-21 21:08:35 +0100

    reader: try-with resources

--- Assignment5/src/utils/Reader.java
+++ Assignment5/src/utils/Reader.java
@@ -1,8 +1,10 @@
 package utils;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+
 public class Reader {
     // Accept alphabet from main
     // Decode alphabet?
@@ -11,30 +13,27 @@ public class Reader {
     // Send grouped logs and alphabet to Automaton?
 
     public HashMap<String, ArrayList<String>> ScanFile(String path) {
-        Scanner sc = null;
-        try {
-            sc = new Scanner(new File(path));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
         HashMap<String, ArrayList<String>> logEntries = new HashMap<>();
-        while (sc.hasNextLine())
-        {
-            String[] arr = sc.nextLine().split(",");
-            if (logEntries.containsKey(arr[2])) {
-                logEntries.get(arr[2]).add(arr[3]);
-            } else {
-                ArrayList<String> tempArr = new ArrayList<>();
-                tempArr.add(arr[3]);
-                logEntries.put(arr[2],tempArr);
+        try (Scanner sc = new Scanner(new File(path))) {
+            while (sc.hasNextLine()) {
+                String[] arr = sc.nextLine().split(",");
+                if (logEntries.containsKey(arr[2])) {
+                    logEntries.get(arr[2]).add(arr[3]);
+                } else {
+                    ArrayList<String> tempArr = new ArrayList<>();
+                    tempArr.add(arr[3]);
+                    logEntries.put(arr[2], tempArr);
+                }
             }
-        }
 //        for (HashMap.Entry<String, ArrayList<String>> entry : logEntries.entrySet()) {
 //            String key = entry.getKey();
 //            ArrayList<String> value = entry.getValue();
 //            System.out.println(key + " " + value);
 //        }
-        sc.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found:" + e);
+        }
         return logEntries;
     }
 }
