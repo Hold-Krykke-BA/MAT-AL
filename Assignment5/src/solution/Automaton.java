209c3123b329c0b4ecf624639ba3f7991f4e9dce
Camilla Staunstrup <camilla.valerius@gmail.com>
2021-03-21 13:31:35 +0100

    Automaton constructer, som Anders'

--- Assignment5/src/solution/Automaton.java
+++ Assignment5/src/solution/Automaton.java
@@ -2,7 +2,9 @@ package solution;
 import interfaces.IAutomaton;
 import interfaces.IState;
 
+import java.util.ArrayList;
 import java.util.List;
+
 
 public class Automaton implements IAutomaton {
     // Accept decoded alphabet (rules)?
@@ -12,10 +14,22 @@ public class Automaton implements IAutomaton {
         // is any instance stuck at a non-final (non-accepting) state
         // give a list of the running instances
 
+    private List<IState> states = new ArrayList<>();
+    private IState initialState;
+
+
+    public Automaton(int stateCount){
+        states.add(initialState = new StateABCDEFG(0, false));
+        for (int index = 1; index < stateCount; index++) {
+            if (index == stateCount-1) states.add(new StateABCDEFG(index, true));
+            else states.add(new StateABCDEFG(index, false));
+        }
+    }
 
 
     @Override
     public List<IState> getStates() {
+
         return null;
     }
 
