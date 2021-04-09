d0b8b7c6e9a0426a30e09b837aa2b2fb1198932b
Runi <Runiniclasen@hotmail.com>
2021-04-09 16:45:22 +0200

    test booleanvalueset

--- Assignment5a/src/Main.java
+++ Assignment5a/src/Main.java
@@ -8,6 +8,7 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Testing RangeValueSet");
         MappedState P = new MappedState(); // precondition
         Map<String, ValueSet> pre = P.getVariables();
 
@@ -31,6 +32,39 @@ public class Main {
         SyntaxTree C = new VSSLSyntaxTree(); // commands / code / program
         State S = C.analyse(P);
         if (S.subStateOf(Q)) {
+            System.out.println("Hurra!");
+        } else {
+            System.out.println("Øv!");
+        }
+
+
+        System.out.println("-------------------------------------------");
+
+
+        System.out.println("Testing BooleanValueSet");
+
+        MappedState P2 = new MappedState(); // precondition
+        Map<String, ValueSet> pre2 = P2.getVariables();
+
+        BooleanValueSet bvs = new BooleanValueSet(false, true);
+        pre2.put("One", bvs);
+        BooleanValueSet bvs2 = new BooleanValueSet(false, false);
+        pre2.put("Two", bvs2);
+
+
+        //Map<String, ValueSet> postConditionVariables = new HashMap<>();
+        MappedState Q2 = new MappedState(); // postcondition
+
+        Map<String, ValueSet> post2 = Q2.getVariables();
+
+        BooleanValueSet bvs3 = new BooleanValueSet(false, true);
+        post2.put("One", bvs3);
+        BooleanValueSet bvs4 = new BooleanValueSet(false, false);
+        post2.put("Two", bvs4);
+
+        SyntaxTree C2 = new VSSLSyntaxTree(); // commands / code / program
+        State S2 = C2.analyse(P);
+        if (S2.subStateOf(Q2)) {
             System.out.println("Hurra!");
         } else {
             System.out.println("Øv!");
