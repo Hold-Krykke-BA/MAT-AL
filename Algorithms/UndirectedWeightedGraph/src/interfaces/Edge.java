14d01c5f9477e82d8909e1216ec00c280d123cc7
Runi <Runiniclasen@hotmail.com>
2021-04-19 13:36:35 +0200

    add Graph

---
+++ Algorithms/UndirectedWeightedGraph/src/interfaces/Edge.java
@@ -0,0 +1,8 @@
+package interfaces;
+
+//Weighted
+public interface Edge extends Comparable<Edge>{
+    int either();
+    int other(int v);
+    float weight();
+}
---
+++ Algorithms/UndirectedWeightedGraph/src/interfaces/GraphFactory.java
@@ -0,0 +1,8 @@
+package interfaces;
+
+import java.io.IOException;
+
+public interface GraphFactory {
+    GraphBuilder getNewBuilder(int verticeCount);
+    Graph readFromFile(String fileName) throws IOException;
+}
