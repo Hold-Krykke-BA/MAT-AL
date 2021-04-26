6d352b0cc96072f5b0dcebf5f2de8667350c7f2b
Runi <Runiniclasen@hotmail.com>
2021-04-26 16:29:59 +0200

    add UF interface

--- AlgorithmsCollection/src/Datastructures/UnionFind/Interfaces/UnionFind.java
+++ AlgorithmsCollection/src/Datastructures/UnionFind/Interfaces/UnionFind.java
@@ -1,4 +1,44 @@
 package Datastructures.UnionFind.Interfaces;
 
-public interface UnionFind {
+/**
+ * Generic type interface. Notice how <code>isConnected()</code> returns boolean and <code>count()</code> returns int.
+ * <p>
+ * Here's a non-interface solution using generics and HashMaps:
+ * https://codereview.stackexchange.com/questions/49186/generic-implementation-of-the-quick-union-algorithm-with-path-compression
+ *
+ * @param <T> Type of choice
+ */
+public interface UnionFind<T> {
+    /**
+     * add connection between p and q
+     *
+     * @param p Element 1
+     * @param q Element 2
+     */
+    void union(T p, T q);
+
+    /**
+     * number of components/sets
+     *
+     * @return int value of structure size
+     */
+    int count();
+
+    /**
+     * component identifier for p ( given point p, return set of p)
+     *
+     * @param p
+     * @return
+     */
+    T findRoot(T p);
+
+    /**
+     * //     * @param  p one element
+     * //     * @param  q the other element
+     * //     * @return true if p and q are in the same set else false.
+     * //
+     */
+    default boolean isConnected(T p, T q) {
+        return findRoot(p) == findRoot(q);
+    }
 }
