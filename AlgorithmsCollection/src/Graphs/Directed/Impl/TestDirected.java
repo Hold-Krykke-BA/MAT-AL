ea249eed9eb3417d07a96875b66469c4f46e7d96
Asger Thorsboe Lundblad <balloupjuske2208@gmail.com>
2021-04-26 18:50:24 +0200

    added Dijksta algorithm

--- AlgorithmsCollection/src/Graphs/Directed/Impl/TestDirected.java
+++ AlgorithmsCollection/src/Graphs/Directed/Impl/TestDirected.java
@@ -2,14 +2,20 @@ package Graphs.Directed.Impl;
 
 import Graphs.Directed.Interfaces.DirectedGraph;
 import Graphs.Directed.Interfaces.DirectedGraphFactory;
+import ShortestPath.Dijkstra.Impl.SpDijkstraImpl;
+import ShortestPath.Dijkstra.Interfaces.SpDijkstra;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class TestDirected {
     public static void main(String[] args) throws IOException {
         DirectedGraphFactory factory = new DirectedGraphFactoryImpl();
         DirectedGraph graph = factory.readFromFile("src/Textfiles/graph2.txt");
         System.out.println(graph);
+        SpDijkstraImpl dijkstra = new SpDijkstraImpl(graph, 0);
+        System.out.println(dijkstra);
+        System.out.println(dijkstra.path(7, new ArrayList<>()));
     }
 
 }
--- AlgorithmsCollection/src/ShortestPath/Dijkstra/Impl/SpDijkstraImpl.java
+++ AlgorithmsCollection/src/ShortestPath/Dijkstra/Impl/SpDijkstraImpl.java
@@ -1,4 +1,93 @@
 package ShortestPath.Dijkstra.Impl;
 
+import Graphs.Directed.Interfaces.DirectedEdge;
+import Graphs.Directed.Interfaces.DirectedGraph;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class SpDijkstraImpl {
+
+    float[] weightArr;
+    int[] pathArr;
+    boolean[] visitedArr;
+    PriorityQueue pq = new PriorityQueue(new WeightComparator());
+
+    public SpDijkstraImpl(DirectedGraph directedGraph, int source) {
+        weightArr = new float[directedGraph.V()];
+        pathArr = new int[directedGraph.V()];
+        visitedArr = new boolean[directedGraph.V()];
+        for (int i = 0; i < directedGraph.V(); i++) {
+            weightArr[i] = Float.MAX_VALUE;
+            pathArr[i] = -1;
+            visitedArr[i] = false;
+        }
+        weightArr[source] = 0;
+        pq.add(source);
+
+        while (!pq.isEmpty()) {
+            int nextPq = (int) pq.poll();
+            for(DirectedEdge e : directedGraph.adj(nextPq)) {
+                if (!visitedArr[e.to()]) {
+                    pq.add(e.to());
+                    visitedArr[e.to()] = true;
+                }
+                float tempWeight = weightArr[nextPq] + e.weight();
+                if (tempWeight < weightArr[e.to()]) {
+                    weightArr[e.to()] = tempWeight;
+                    pathArr[e.to()] = e.from();
+                }
+            }
+        }
+    }
+
+    public String path(int to, ArrayList<Integer> pathArr) {
+        if (this.pathArr[to] == -1) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("Shortest path to " + pathArr.get(0) + "\n");
+            for(int i = pathArr.size()-1; i >= 0; i--) {
+                if (i == 0) {
+                    builder.append(pathArr.get(i));
+                } else {
+                    builder.append(pathArr.get(i) + " -> ");
+                }
+            }
+            return builder.toString();
+        }
+        if (pathArr == null) {
+            pathArr = new ArrayList<>();
+        }
+        if (pathArr.isEmpty()) {
+            pathArr.add(to);
+        }
+        pathArr.add(this.pathArr[to]);
+        return path(this.pathArr[to], pathArr);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("   Weight | Path \n");
+        for (int i = 0; i < weightArr.length; i++) {
+            builder.append(i + ": ");
+            builder.append(weightArr[i]);
+            builder.append("    | ");
+            builder.append(pathArr[i]);
+            builder.append("\n");
+        }
+        return builder.toString();
+    }
+
+    class WeightComparator implements Comparator<Integer> {
+
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            float f = weightArr[o1] - weightArr[o2];
+            if (f > 0) return 1;
+            if (f < 0) return -1;
+            return 0;
+        }
+    }
+
 }
